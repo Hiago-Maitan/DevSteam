d757e319089850921b4443135ee6d12583d6b113
Maitann <hiagomaitan@gmail.com>
2023-07-22 03:19:40 -0300

    FEAT: styles GameCard component

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,10 @@ export default function Home() {
           </div>
           <div className={styles.session}>
             <Subtitle>Outros jogos</Subtitle>
-            <div>
+            <div className={styles.gameContainer}>
+              <GameCard/>
+              <GameCard/>
+              <GameCard/>
               <GameCard/>
             </div>
           </div>
