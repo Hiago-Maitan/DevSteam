7d1eb84e42cf65a111b4a985cf8ea1aeda24fcf2
Maitann <hiagomaitan@gmail.com>
2023-07-28 02:46:52 -0300

    FEAT: props values

--- src/components/cards/saleCard/saleCard.jsx
+++ src/components/cards/saleCard/saleCard.jsx
@@ -2,17 +2,17 @@ import Image from 'next/image';
 import styles from './saleCard.module.css'
 import Button from '@/components/forms/button/button';
 
-function SaleCard() {
+function SaleCard({image, discount, fullPrice, discountPrice}) {
   return ( 
     <div className={styles.saleCard}>
-      <Image src="/products/league-of-legends.jpg" width={250} height={300}/>
+      <Image src={`/products/${image}`} alt={image} width={250} height={300}/>
       <div className={styles.info}>
         <h3 className={styles.title}>Oferta Exclusiva</h3>
         <div className={styles.priceCard}>
-          <div className={styles.percent}>-20%</div>
+          <div className={styles.percent}>-{discount}%</div>
           <div className={styles.price}>
-            <p>R$99,90</p>
-            <h4>R$89,90</h4>
+            <p>R${fullPrice}</p>
+            <h4>R${discountPrice}</h4>
           </div>
         </div>
        <Button fullWidth>Adicionar ao carrinho</Button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,18 +21,33 @@ export default function Home() {
           <div className={styles.session}>
             <Subtitle>Promoções</Subtitle>
             <div className={styles.saleContainer}>
-              <SaleCard />
-              <SaleCard />
-              <SaleCard />
+              <SaleCard
+                image={"league-of-legends.jpg"}
+                discount={75}
+                fullPrice={"79,99"}
+                discountPrice={"39,99"}
+              />
+              <SaleCard
+                image={"dota-2.jpg"}
+                discount={30}
+                fullPrice={"19,99"}
+                discountPrice={"9,99"}
+              />
+              <SaleCard
+                image={"valorant.jpg"}
+                discount={50}
+                fullPrice={"30,00"}
+                discountPrice={"15,99"}
+              />
             </div>
           </div>
           <div className={styles.session}>
             <Subtitle>Outros jogos</Subtitle>
             <div className={styles.gameContainer}>
-              <GameCard/>
-              <GameCard/>
-              <GameCard/>
-              <GameCard/>
+              <GameCard />
+              <GameCard />
+              <GameCard />
+              <GameCard />
             </div>
           </div>
         </Container>
