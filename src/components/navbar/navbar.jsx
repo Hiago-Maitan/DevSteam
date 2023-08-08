2a45c133d6198bfe6a2025af7936418aaf87f480
Maitann <hiagomaitan@gmail.com>
2023-08-08 03:06:06 -0300

    FEAT: add warning about the prices

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,13 @@
 import styles from "./navbar.module.css"
 
 import Logo from "../logo/logo"
-import Input from "../forms/input/input"
 import CartButton from "../cartButton/cartButton"
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Logo />
+      <h4 className={styles.warning}>valores meramente ilustrativos</h4>
       <CartButton/>
     </nav>
   )
